1aa10fe78111720e9f41d6d0b3930c5905d1dad7
Bhavesh Salunke <bhavesh.salunke@massiverocket.com>
2025-07-19 14:37:17 +0530

    Refactor content file paths and rename markdown files

--- src/shared/content/Content.tsx
+++ src/shared/content/Content.tsx
@@ -7,9 +7,6 @@ import common from "content/common/common.json";
 import landing from "content/landing/landing-config.json";
 import about from "content/about/about-config.json";
 
-import LandingMd from "content/landing/landing.md";
-import AboutMd from "content/about/about.md";
-
 export const configs = {
     common,
     landing,
@@ -27,8 +24,8 @@ export enum MarkdownFile {
 }
 
 const Mapper = {
-    [MarkdownFile.Landing]: LandingMd,
-    [MarkdownFile.About]: AboutMd,
+    [MarkdownFile.Landing]: "/content/landing.md",
+    [MarkdownFile.About]: "/content/about.md",
 };
 
 export const useContent = (fileName: MarkdownFile) => {
