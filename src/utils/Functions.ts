d92ee7576a19e7e31fd1aef3dd62734c5146d802
Bhavesh Salunke <bhavesh.salunke@massiverocket.com>
2025-07-20 18:47:38 +0530

    Add dataLayer event tracking for resume clicks

--- src/utils/Functions.ts
+++ src/utils/Functions.ts
@@ -3,6 +3,9 @@ import { configs } from "shared/content/Content";
 export const open = (link: string) => window.open(link, "_blank");
 
 export const onResumeOpen = () => {
+    if (typeof window !== "undefined" && window.dataLayer) {
+        window.dataLayer.push({ event: "resume_click" });
+    }
     open(configs.common.resume);
 };
 
